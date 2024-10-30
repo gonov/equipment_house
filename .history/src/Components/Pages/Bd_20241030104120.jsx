94f9bcbf7d11f8367a7f0eee3f6aa7d87de5fb99
gonov <adamej10@gmail.com>
2024-10-30 12:50:49 +0300

    dev1

---
+++ .history/src/Components/Pages/Bd_20241030104120.jsx
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: 'Мясорубкdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddа f',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 2,
+    name: 'Мясорубка 10dddddddddddddddddddddddddddddddddddddddddddddd00ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 3,
+    name: 'Мясорубка 1000ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 4,
+    name: 'Мясорубка 1000ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+];
+
+export default products;
+
+
+const busSol = [
+    {
+        id:1,
+        name: "для столовых",
+    price:    }
+]
---
+++ .history/src/Components/Pages/Bd_20241030104649.jsx
@@ -0,0 +1,58 @@
+const products = [
+  {
+    id: 1,
+    name: 'Мясорубкdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddа f',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 2,
+    name: 'Мясорубка 10dddddddddddddddddddddddddddddddddddddddddddddd00ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 3,
+    name: 'Мясорубка 1000ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+  {
+    id: 4,
+    name: 'Мясорубка 1000ватт',
+    price: 1000,
+    img: '/images/123.png',
+  },
+];
+
+export default products;
+
+{
+  /*  данные для готовых решений для бизнеса */
+}
+
+const busSol = [
+  {
+    id: 1,
+    name: 'для столовых',
+    price: 45000,
+    img: ''
+  },
+  {
+    id: 2,
+    name: 'для кафе',
+    price: 45000,
+  },
+  {
+    id: 3,
+    name: 'для ресторанов',
+    price: 45000,
+  },
+  {
+    id: 4,
+    name: 'для пекарни',
+    price: 45000,
+  },
+];
+
+export default busSol;
