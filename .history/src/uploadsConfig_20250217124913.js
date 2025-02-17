21b90169aeff9a3e3472ff4de158b97688034dc6
gonov <adamej10@gmail.com>
2025-02-17 12:56:30 +0300

    123

--- .history/src/uploadsConfig_20250217124913.js
+++ .history/src/uploadsConfig_20250217124913.js
@@ -1,5 +1,5 @@
 function uploadsConfig() {
-	// const server = 'http://localhost:3000'
+	const server = 'http://localhost:3000'
 	  const server = 'https://backend.xn--09-6kcdfljx8bfbby6b.xn--p1ai'
 	return server
 }
